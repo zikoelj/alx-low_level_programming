7ee3a457f4efef5bd1e4a6f46e480cf7ea9c5792
zikoelj <zakariaelj24@gmail.com>
2023-03-16 18:31:16 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
-int main(void){
+/**
+*main -main entry point
+*return : always 0 (success)
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
