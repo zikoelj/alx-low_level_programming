e4f9f969875e5219f575089caeeccdb3dfb7b15d
zikoelj <zakariaelj24@gmail.com>
2023-03-17 14:25:17 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+char c='z';
+while(c>='a')
+{	
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
+
