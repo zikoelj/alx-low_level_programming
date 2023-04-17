c7f2218627a09439df175630b68e9c94803d0506
zikoelj <zakariaelj24@gmail.com>
2023-04-17 16:58:09 -0700

    /0x0E-structures_typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
+
 /**
  * init_dog - Entry point
  * @d: passing structure at the function
@@ -9,10 +10,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d!=NULL)
+	/* Passing to stucture from main */
+	if (d != NULL)
 	{
-		d->name=name;
-		d->age=age;
-		d->owner=owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
