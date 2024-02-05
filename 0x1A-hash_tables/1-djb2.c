8aa1f9e104b885732eb70b67ce35edb149507098
zikoelj <zakariaelj24@gmail.com>
2024-02-04 18:10:54 -0800

    good with hashtable

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implimentation of djb2 aldo
+ * @str: The array
+ *
+ * Return: A pointer to the newly created hash table, or NULL if it fails
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+unsigned long int hash = 5381;
+int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+while ((c = *str++))
+{
+hash = ((hash << 5) + hash) +c;
+}
+
+return (hash);
 }
